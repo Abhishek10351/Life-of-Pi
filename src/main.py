5955cc17aeb480f85432271ee80dd49c6ef1c805
Nicolas <90798171+Pegaa@users.noreply.github.com>
2022-09-06 20:34:09 +0200

    Feat: adding the win/loose mechanic

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import arcade
 
 from config import SCREEN_HEIGHT, SCREEN_TITLE, SCREEN_WIDTH
-from window import Menu, Window
+from window import Menu, Window, WinLooseMenu
 
 if __name__ == '__main__':
     game = Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     menu = Menu(game)
+    win_loose = WinLooseMenu(game)
     game.show_view(menu)
     arcade.run()
--- src/window/__init__.py
+++ src/window/__init__.py
@@ -1,8 +1,9 @@
-from window.views import Game, Menu
+from window.views import Game, Menu, WinLooseMenu
 from window.window import Window
 
 __all__ = [
     "Game",
     "Menu",
-    "Window"
+    "Window",
+    "WinLooseMenu"
 ]
